4d29d9edfc0845f13c559cf1a1ee50e649f95d45
Ramdani <dannymld41@gmail.com>
2025-03-19 06:23:21 +0700

    Setting up

---
+++ ClassManagementApp/ClassManagementApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassManagementApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ClassManagementApp/ClassManagementApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ClassManagementApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
